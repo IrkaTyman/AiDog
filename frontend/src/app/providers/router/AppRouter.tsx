1b9fd4a289916d48767a386f1683a8d9f7712f96
Irina <tymanira@mail.ru>
2024-04-27 20:17:34 +0500

    [F] feat(toutes) - add protected routes

--- frontend/src/app/providers/router/AppRouter.tsx
+++ frontend/src/app/providers/router/AppRouter.tsx
@@ -1,13 +1,17 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { useAuthContext } from '@app/providers/AuthProvider/AuthProvider';
+
 import { routeConfig } from './config';
 
 export const AppRouter = () => {
+    const { isAuth } = useAuthContext();
+
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                {routeConfig.map(route => (
+                {routeConfig(isAuth).map(route => (
                     <Route
                         {...route}
                         key={route.path}
--- frontend/src/app/providers/router/config.tsx
+++ frontend/src/app/providers/router/config.tsx
@@ -1,13 +1,28 @@
-import { RouteProps } from 'react-router-dom';
+import { RouteProps, Navigate } from 'react-router-dom';
 
 import { authRouteConfig } from '@pages/auth/routes';
 import { recordRouteConfig } from '@pages/records/routes';
 
-export const routeConfig: RouteProps[] = [
-    {
-        path: '*',
-        element: <p style={{ color: 'red' }}>Not found page</p>,
-    },
-    ...authRouteConfig,
-    ...recordRouteConfig,
-];
+export const routeConfig = (isAuth: boolean): RouteProps[] => {
+    let routes: RouteProps[] = [];
+
+    if (isAuth) {
+        routes = routes.concat([
+            ...recordRouteConfig,
+            {
+                path: '*',
+                element: <Navigate to={'/record'} />,
+            },
+        ]);
+    } else {
+        routes = routes.concat([
+            ...authRouteConfig,
+            {
+                path: '*',
+                element: <Navigate to={'/login'} />,
+            },
+        ]);
+    }
+
+    return routes;
+};
